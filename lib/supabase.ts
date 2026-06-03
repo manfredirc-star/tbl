635b101afabad0da0a82bf318613078ed9451687
Manfred <manfredirc@gmail.com>
2026-06-03 17:56:51 +0200

    update site

--- lib/supabase.ts
+++ lib/supabase.ts
@@ -2,5 +2,5 @@ import { createClient } from "@supabase/supabase-js";
 
 export const supabase = createClient(
   "https://vyfzlqnlzhmjmfwarzsa.supabase.co",
-  "sb_publishable_AxovgF-bbcN-tVQJGI1dTw_vXEdvsMQ"
+  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InZ5ZnpscW5semhtam1md2FyenNhIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODA0NzIwMDcsImV4cCI6MjA5NjA0ODAwN30.hLUU8cn0GFb7QLKNAnGVfOprk274y521mQVTDBTHZM4"
 );
